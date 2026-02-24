ca8a83f8d97ff6042a76a4401c07b4a1d7ae7972
hasbifadillah <hasbifadillah@outlook.com>
2026-02-24 21:16:44 +0700

    fix: build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,21 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import App from './App'
+import { render, screen } from "@testing-library/react";
+// import userEvent from '@testing-library/user-event'
+import App from "./App";
+import { describe, expect, it } from "vitest";
 
-describe('App', () => {
-  it('renders main sections', () => {
-    render(<App />)
+describe("App", () => {
+  it("renders main sections", () => {
+    render(<App />);
 
-    expect(screen.getByRole('heading', { name: /about/i })).toBeInTheDocument()
+    expect(screen.getByRole("heading", { name: /about/i })).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /skills/i }),
-    ).toBeInTheDocument()
+      screen.getByRole("heading", { name: /skills/i }),
+    ).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /projects/i }),
-    ).toBeInTheDocument()
+      screen.getByRole("heading", { name: /projects/i }),
+    ).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /contact/i }),
-    ).toBeInTheDocument()
-  })
-})
-
+      screen.getByRole("heading", { name: /contact/i }),
+    ).toBeInTheDocument();
+  });
+});
